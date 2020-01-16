a04fe2097a4744319d4149fef40f7d6a972e6c91
CarlosCL98 <camer.cl98@gmail.com>
2020-01-16 11:20:15 -0500

    Añadiendo nuevos productos a la tienda.

--- src/components/Products.js
+++ src/components/Products.js
@@ -11,41 +11,33 @@ export default class Products extends React.Component {
           price: 1000000,
           image: "https://media.aws.alkosto.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/n/i/nintendoswitchneon.jpg"
         },
-        { name: "Nintendo Switch",
-          price: 1000000,
-          image: "https://media.aws.alkosto.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/n/i/nintendoswitchneon.jpg"
+        { name: "Xbox One",
+          price: 1500000,
+          image: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3SQnt?ver=5797&q=90&m=6&h=1445&w=1849&b=%23FFFFFFFF&f=jpg&o=f&aim=true"
         },
-        { name: "Nintendo Switch",
-          price: 1000000,
-          image: "https://media.aws.alkosto.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/n/i/nintendoswitchneon.jpg"
+        { name: "PlayStation 4",
+          price: 1350000,
+          image: "http://www.costco.com.mx/medias/sys_master/products/hca/h71/11253861908510.jpg"
         },
-        { name: "Nintendo Switch",
-          price: 1000000,
-          image: "https://media.aws.alkosto.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/n/i/nintendoswitchneon.jpg"
+        { name: "God Of War Game PS4",
+          price: 80000,
+          image: "https://static.raru.co.za/cover/2019/10/09/7821671-m.jpg?v=1570786535"
         },
-        { name: "Nintendo Switch",
-          price: 1000000,
-          image: "https://media.aws.alkosto.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/n/i/nintendoswitchneon.jpg"
+        { name: "Legend Of Zelda BOTW Switch",
+          price: 175000,
+          image: "https://media.vandal.net/m/43030/the-legend-of-zelda-breath-of-the-wild-201732131429_1.jpg"
         },
-        { name: "Nintendo Switch",
-          price: 1000000,
-          image: "https://media.aws.alkosto.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/n/i/nintendoswitchneon.jpg"
+        { name: "Dragon Ball Z Kakarot PS4",
+          price: 230000,
+          image: "http://www.gamecored.com/wp-content/uploads/2019/12/Screen-Shot-2019-12-08-at-5.30.24-PM.png"
         },
-        { name: "Nintendo Switch",
-          price: 1000000,
-          image: "https://media.aws.alkosto.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/n/i/nintendoswitchneon.jpg"
+        { name: "The Binding of Issac Switch",
+          price: 75000,
+          image: "https://www.mobygames.com/images/covers/l/387006-the-binding-of-isaac-afterbirth-nintendo-switch-inside-cover.jpg"
         },
-        { name: "Nintendo Switch",
-          price: 1000000,
-          image: "https://media.aws.alkosto.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/n/i/nintendoswitchneon.jpg"
-        },
-        { name: "Nintendo Switch",
-          price: 1000000,
-          image: "https://media.aws.alkosto.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/n/i/nintendoswitchneon.jpg"
-        },
-        { name: "Nintendo Switch",
-          price: 1000000,
-          image: "https://media.aws.alkosto.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/n/i/nintendoswitchneon.jpg"
+        { name: "Minecraft PC Edition",
+          price: 50000,
+          image: "https://http2.mlstatic.com/minecraft-cover-fkt-pc-D_NQ_NP_659358-MLA32116104277_092019-F.jpg"
         }
       ]
     }
